55dce1706cbcb23c368ccc5ef3fd8bb0712bae02
Minoru Otani <otani@ccs.tsukuba.ac.jp>
2022-05-27 16:16:59 +0900

    use own xsd

--- generate_qes_files.py
+++ generate_qes_files.py
@@ -4,9 +4,11 @@ from collections import deque
 from glob import glob
 
 #schema = '../qeschemas/PW_CPV/test_schemas/qes_211101.xsd' 
-schema_test = './tests/schemas/qe/qes-refactored.xsd' 
+#schema_test = './tests/schemas/qe/qes-refactored.xsd' 
+schema_test = '../qeschemas/PW_CPV/qes_current_master.xsd'
 temps = map(pathlib.Path,glob('src/xsdtools/codes/templates/qe/*/qes_*_module.f90.jinja'))
 
 g1 = ( (xsdtools.QEFortranGenerator(schema_test, p.parent ),p.name) for p in temps) 
-g2 = (_[0].render_to_files(_[1], force=True) for _ in g1)
+#g2 = (_[0].render_to_files(_[1], force=True) for _ in g1)
+g2 = (_[0].render_to_files(_[1], output_dir='./output', force=True) for _ in g1)
 deque(g2, maxlen=0)
